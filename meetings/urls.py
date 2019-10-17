fa6ba83497acef2becb2ff2a3c27f0decd579575
Filip 'Cherit' Szóstak <filip-szostak@wp.pl>
2019-10-17 17:29:40 +0200

    New routes system merged with meetings

--- meetings/urls.py
+++ meetings/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.urls import path, include
+from qr_code import urls as qr_code_urls
+
 
 from . import views
 
@@ -6,4 +8,9 @@ urlpatterns = [
     path('create/', views.create, name="meeting_create"),
     path('', views.MeetingListView.as_view(), name="meeting_list"),
     path('<int:pk>/', views.MeetingDetailView.as_view(), name="meeting_view"),
+    path('activate/<int:pk>/', views.activate, name="meeting_activate"),
+    path('register/', views.register, name="meeting_register"),
+    path('register/', include(qr_code_urls, namespace='qr_code')),
+    path('register/<str:code>/', views.register, name="meeting_register_code"),
+
 ]
