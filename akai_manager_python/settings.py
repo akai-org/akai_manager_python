e0974007d8ea87b9e8cfada30623cc569115d57e
Jorewin <blazejowski.j@gmail.com>
2019-11-03 23:51:46 +0100

    Added favicon for most platforms

--- akai_manager_python/settings.py
+++ akai_manager_python/settings.py
@@ -164,6 +164,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/dist')
+    os.path.join(BASE_DIR, 'static/dist'),
+    'static/images',
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
