6973352e9d83bccec55c6656a82233bb4b6144f2
Filip 'Cherit' Szóstak <filip-szostak@wp.pl>
2019-12-04 17:22:42 +0100

    API endpoints created. Models & serializers finnished

--- cms/models.py
+++ cms/models.py
@@ -5,21 +5,28 @@ from django.contrib.auth.models import User
 class Image(models.Model):
     file = models.ImageField()
     name = models.TextField(null=False)
+    url = models.TextField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.url}'
 
 class Tag(models.Model):
     name = models.TextField(null=False)
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Article(models.Model):
     title = models.TextField(null=False)
     content = models.TextField(null=False)
     description = models.TextField(null=True)
 
-    cover_image = models.OneToOneField(Image, on_delete=models.CASCADE())
-    author = models.OneToOneField(User, on_delete=models.DO_NOTHING())
+    cover_image = models.TextField(null=False)
+    author = models.OneToOneField(User, on_delete=models.DO_NOTHING)
 
-    images = models.ManyToManyField(Image, on_delete=models.DO_NOTHING())
-    tags = models.ManyToManyField(Tag, on_delete=models.DO_NOTHING())
+    images = models.ManyToManyField(Image)
+    tags = models.ManyToManyField(Tag)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- cms/urls.py
+++ cms/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
-
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    # path('create/', views.create, name="meeting_create"),
+    path('api/articles/', views.ArticleListViewSet.as_view({'get': 'list'}), name="article_list"),
+    path('api/articles/<int:key>/', views.ArticleViewSet.as_view({'get': 'list'}), name="article_list"),
 ]
