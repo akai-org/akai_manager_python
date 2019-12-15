caa23d4015954facc54007b42fbe9c4e8c4dd0fa
Filip 'Cherit' Szóstak <filip-szostak@wp.pl>
2019-12-15 13:43:46 +0100

    Server fixes

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ import os
 
 import environ
 env = environ.Env(
-    DEBUG=(bool, False)
+    DEBUG=(bool, True)
 )
 # reading .env file
 environ.Env.read_env()
@@ -25,13 +25,15 @@ DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-ALLOWED_HOSTS = ['localhost', 'YouAreInWrongPlace.kei.keint.com'
+ALLOWED_HOSTS = [
+    'localhost',
+    'manager.akai.org.pl'
                  ]
 # Application definition
 
@@ -104,7 +106,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'akai_manager_python/templates'),
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -179,14 +181,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/dist'),
-    'static/images',
-    'static/dist/'
+    os.path.join(BASE_DIR, 'static/images'),
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'akai_manager_python/static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
---
+++ update.sh
@@ -0,0 +1,5 @@
+git pull
+./venv/bin/pip install -r requirements.txt
+./venv/bin/python manage.py migrate
+npm install
+npm run update
