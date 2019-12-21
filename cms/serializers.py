d77f53d3ef0d83349f5a7ba365b44193f71fdb47
Filip 'Cherit' Szóstak <filip-szostak@wp.pl>
2019-12-21 14:48:58 +0100

    Backend Article view

--- cms/serializers.py
+++ cms/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Article
 from django.contrib.auth.models import User
+from html import unescape
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -14,13 +15,43 @@ class ArticleSerializer(serializers.HyperlinkedModelSerializer):
     tags = serializers.StringRelatedField(many=True)
     images = serializers.StringRelatedField(many=True)
     cover_image = serializers.StringRelatedField()
+    content = serializers.SerializerMethodField('get_content')
 
     class Meta:
         model = Article
-        fields = ['title', 'content', 'description', 'cover_image', 'author', 'tags', 'images', 'created_at', 'updated_at']
+        fields = ['title', 'content', 'description', 'cover_image', 'author', 'tags', 'images', 'created_at',
+                  'updated_at']
+
+    def create(self, validated_data):
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.description = validated_data.get('description', instance.description)
+        instance.cover_image = validated_data.get('cover_image', instance.cover_image)
+        instance.author = validated_data.get('author', instance.author)
+        instance.save()
+        return instance
+
+    def get_content(self, obj):
+        if self.context.get('escape'):
+            return obj.content
+        else:
+            return unescape(obj.content)
 
 
 class UpdateArticleListSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Article
         fields = ['id', 'updated_at']
+
+
+class ArticleListSerializer(serializers.HyperlinkedModelSerializer):
+    author = AuthorSerializer(read_only=True)
+    cover_image = serializers.StringRelatedField()
+
+    class Meta:
+        model = Article
+        fields = ['title', 'description', 'cover_image', 'author', 'created_at', 'updated_at']
+
--- cms/views.py
+++ cms/views.py
@@ -1,29 +1,79 @@
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
-from rest_framework.decorators import permission_classes, authentication_classes
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import status
 
 from django.shortcuts import get_object_or_404
 
-from .serializers import ArticleSerializer, UpdateArticleListSerializer
+from .serializers import ArticleSerializer, UpdateArticleListSerializer, ArticleListSerializer
 from .models import Article
+
+
+class ArticleSetPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = UpdateArticleListSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes_by_action = {
+        'create': IsAuthenticated,
+    }
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
     def list(self, request, **kwargs):
-        queryset = Article.objects.all()
-        serializer = UpdateArticleListSerializer(queryset, many=True)
+        queryset = Article.objects.all().order_by('-created_at')
+        if request.user.is_authenticated and request.user.groups.filter(name='Publisher').exists():
+            self.pagination_class = ArticleSetPagination
+            serializer = ArticleListSerializer(queryset, many=True)
+        else:
+            self.pagination_class = None
+            serializer = UpdateArticleListSerializer(queryset, many=True)
         return Response(serializer.data)
 
+    def create(self, request, **kwargs):
+        if request.user.is_authenticated and request.user.has_perm('manage_articles'):
+            serializer = ArticleListSerializer(data=request.data)
+            return Response(serializer.data)
+        else:
+            response = {'error': 'User has no permissions to perform this action'}
+            return Response(response, status=status.HTTP_403_FORBIDDEN)
+
     def retrieve(self, request, pk=None, **kwargs):
-        queryset = Article.objects.all()
-        article = get_object_or_404(queryset, pk=pk)
-        serializer = ArticleSerializer(article)
+        article = get_object_or_404(Article, pk=pk)
+        if self.request.query_params.get('noescape'):
+            serializer = ArticleSerializer(article, context={'escape': False})
+        else:
+            serializer = ArticleSerializer(article, context={'escape': True})
         return Response(serializer.data)
+
+    def partial_update(self, request, pk=None, **kwargs):
+        article = get_object_or_404(Article, pk=pk)
+        if request.user.is_authenticated and request.user.has_perm('manage_articles'):
+            serializer = ArticleListSerializer(article, data=request.data)
+            return Response(serializer.data)
+        else:
+            response = {'error': 'User has no permissions to perform this action'}
+            return Response(response, status=status.HTTP_403_FORBIDDEN)
+
+    def destroy(self, request, pk=None, **kwargs):
+        article = get_object_or_404(Article, pk=pk)
+        if request.user.is_authenticated and request.user.has_perm('manage_articles'):
+            article.delete()
+            response = {'success': 'Article deleted successfully'}
+            return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = {'error': 'User has no permissions to perform this action'}
+            return Response(response, status=status.HTTP_403_FORBIDDEN)
+
+    def get_permissions(self):
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError:
+            return [permission() for permission in self.permission_classes]
